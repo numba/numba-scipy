bbf1fd0ecef783bf4451f132483b53404e832c59
Stuart Archibald <stuart.archibald@googlemail.com>
2019-09-17 13:25:04 +0100

    Add reference documentation for scipy.special functions.
    As title, also moves code generation scripts to "scripts"

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,6 +56,8 @@ pygments_style = 'sphinx'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.autodoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
--- docs/source/reference/index.rst
+++ docs/source/reference/index.rst
@@ -3,3 +3,9 @@ Reference Manual
 ================
 
 The ``numba-scipy`` reference manual.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   special.rst
