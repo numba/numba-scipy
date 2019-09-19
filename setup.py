d6672fea6abf0eb7d58c6fe725e614285e834b42
Stuart Archibald <stuart.archibald@googlemail.com>
2019-09-19 16:28:12 +0100

    Prefer setuptools.find_packages()
    As title

--- setup.py
+++ setup.py
@@ -1,28 +1,8 @@
-from setuptools import setup
-import os
+from setuptools import setup, find_packages
 import versioneer
 
+
 _install_requires = ['scipy>=0.16', 'numba>=0.45']
-
-
-def find_packages(root_dir, root_name):
-    """
-    Recursively find packages in *root_dir*.
-    """
-    packages = []
-
-    def rec(path, pkg_name):
-        packages.append(pkg_name)
-        for fn in sorted(os.listdir(path)):
-            subpath = os.path.join(path, fn)
-            if os.path.exists(os.path.join(subpath, "__init__.py")):
-                subname = "%s.%s" % (pkg_name, fn)
-                rec(subpath, subname)
-    rec(root_dir, root_name)
-    return packages
-
-
-packages = find_packages("numba_scipy", "numba_scipy")
 
 
 metadata = dict(
@@ -48,14 +28,16 @@ metadata = dict(
     author_email="numba-users@continuum.io",
     url="https://github.com/numba/numba-scipy",
     download_url="https://github.com/numba/numba-scipy",
-    packages=packages,
+    packages=find_packages(),
     setup_requires=[],
     install_requires=_install_requires,
     license="BSD",
     zip_safe=False,
 )
 
+
 with open('README.rst') as f:
     metadata['long_description'] = f.read()
+
 
 setup(**metadata)
