e165994247d8b73a6094c930c24b2f2b62d6bac4
luk-f-a <lucioarjona@hotmail.com>
2020-01-26 23:25:58 +0100

    adding stats documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,7 +58,6 @@ pygments_style = 'sphinx'
 extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
