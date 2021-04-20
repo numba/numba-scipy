8474820c035cef90c24da04099bc9e77d8f40b1f
Valentin Haenel <vhaenel@anaconda.com>
2021-04-20 19:16:11 +0200

    unit tests for the signature creation functions
    Adding some simple unit-tests to ensure the signature generating
    functions are all behaving as expected.

--- numba_scipy/tests/test_special.py
+++ numba_scipy/tests/test_special.py
@@ -2,12 +2,19 @@ import itertools
 import warnings
 
 import pytest
+from unittest.mock import patch, Mock
 
 import numpy as np
 from numpy.testing import assert_allclose
 import numba
+from numba.types import float64
 import scipy.special as sc
 from numba_scipy.special import signatures as special_signatures
+from numba_scipy.special.signatures import (parse_capsule_name,
+                                            de_mangle_function_name,
+                                            get_signatures_from_pyx_capi,
+                                            generate_signatures_dicts,
+                                           )
 
 NUMBA_TYPES_TO_TEST_POINTS = {
     numba.types.float64: np.array(
@@ -59,6 +66,84 @@ def get_parametrize_arguments():
     for name, specializations in signatures:
         for signature in specializations:
             yield name, signature
+
+
+def test_parse_capsule_name():
+    input_capsule = '<capsule object "double (double, double, double, int __pyx_skip_dispatch)" at 0x7f8c8d5f5150>'
+    expected = ['double', 'double', 'double', 'double']
+    received = parse_capsule_name(input_capsule)
+    assert received == expected
+
+
+def test_parse_capsule_name_with_invalid_capsule():
+    with pytest.raises(ValueError) as excinfo:
+        input_capsule = '<TESTING object "double (double)" at 0x7f>'
+        parse_capsule_name(input_capsule)
+    assert "Unexpected capsule name" in str(excinfo.value)
+
+
+def test_parse_capsule_name_with_invalid_signature():
+    with pytest.raises(ValueError) as excinfo:
+        input_capsule = '<capsule object "TESTING" at 0x7f>'
+        parse_capsule_name(input_capsule)
+    assert "Unexpected signature" in str(excinfo.value)
+
+
+def test_de_mange_function_name():
+    mangled_name = "__pyx_fuse_0pdtr"
+    received = de_mangle_function_name(mangled_name)
+    expected = "pdtr"
+    assert expected == received
+
+
+def test_de_mange_function_name_with_invalid_name():
+    with pytest.raises(ValueError) as excinfo:
+        # The empty string was the only thing that the regex didn't recognise.
+        mangled_name = ""
+        print(de_mangle_function_name(mangled_name))
+    assert "Unexpected mangled name" in str(excinfo.value)
+
+
+@patch("numba_scipy.special.signatures.cysc")
+def test_get_signatures_from_pyx_capi(cysc_mock):
+    capsule = ('<capsule object "double (double, double, double, '
+                     'int __pyx_skip_dispatch)" at 0x7f8c8d5f5150>')
+    mangled_name = "__pyx_fuse_0pdtr"
+    cysc_mock.__pyx_capi__ = {
+        mangled_name: capsule
+    }
+    expected = {
+        ('__pyx_fuse_0pdtr', float64, float64, float64, float64):
+        ('<capsule object "double (double, double, double, '
+         'int __pyx_skip_dispatch)" at 0x7f8c8d5f5150>')
+    }
+    received = get_signatures_from_pyx_capi()
+    assert expected == received
+
+
+@patch("numba_scipy.special.signatures.get_cython_function_address", Mock())
+@patch("numba_scipy.special.signatures.ctypes.CFUNCTYPE",
+       Mock(return_value=Mock(return_value='0123456789')))
+def test_generate_signatures_dicts():
+    signature_to_pointer = {
+        ('__pyx_fuse_0pdtr', float64, float64, float64, float64):
+        ('<capsule object "double (double, double, double, '
+         'int __pyx_skip_dispatch)" at 0x7f8c8d5f5150>')
+    }
+    expected = ({'pdtr': ((float64, float64, float64),)},
+                {('pdtr', float64, float64, float64): "0123456789"})
+    received = generate_signatures_dicts(signature_to_pointer)
+    assert expected == received
+
+
+def test_ensure_signatures_generated():
+    from numba_scipy.special.signatures import (name_to_numba_signatures,
+                                                name_and_types_to_pointer,
+                                                signature_to_pointer,
+                                                )
+    assert len(name_to_numba_signatures) != 0
+    assert len(name_and_types_to_pointer) != 0
+    assert len(signature_to_pointer) != 0
 
 
 @pytest.mark.parametrize(
